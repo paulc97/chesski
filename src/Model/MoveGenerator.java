fde1bd0837a4128691e1f905515f78a6bd7ad640
tgoldboom <tgoldboom@googlemail.com>
2022-05-01 14:34:40 +0200

    Updated move generator with dummy method

--- src/Model/MoveGenerator.java
+++ src/Model/MoveGenerator.java
@@ -1,4 +1,11 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class MoveGenerator {
+
+    public static ArrayList generateMoves (Game game) {
+        return new ArrayList<>();
+    }
+
 }
