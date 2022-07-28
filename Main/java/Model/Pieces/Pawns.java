ee2a4700469fa9cb58e00bd9c60f717ab69b46d8
tgoldboom <tgoldboom@googlemail.com>
2022-07-28 17:43:12 +0200

    Refactoring for readibility

--- Main/java/Model/Pieces/Pawns.java
+++ Main/java/Model/Pieces/Pawns.java
@@ -8,157 +8,239 @@ public class Pawns implements Piece {
 
     public String moves(Board b) {
 
-        String list = "";
+        String moveList = "";
 
         if (b.isCurrentPlayerIsWhite()) {
-            //x1,y1,x2,y2
-            long PAWN_MOVES = (b.getWhitePawns() >> 7) & (b.getBlackPieces() & ~b.getBlackKing()) & ~RANK_8 & ~FILE_A;//capture right
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i / 8 + 1) + (i % 8 - 1) + (i / 8) + (i % 8);
-                    //TODO discuss: create a new board here, assess the value of it and add it to the result list?
-                }
-            }
-            PAWN_MOVES = (b.getWhitePawns() >> 9) & (b.getBlackPieces() & ~b.getBlackKing()) & ~RANK_8 & ~FILE_H;//capture left
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i / 8 + 1) + (i % 8 + 1) + (i / 8) + (i % 8);
-                }
-            }
-            PAWN_MOVES = (b.getWhitePawns() >> 8) & b.getEmptyFields() & ~RANK_8;//move 1 forward
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i / 8 + 1) + (i % 8) + (i / 8) + (i % 8);
-                }
-            }
-            PAWN_MOVES = (b.getWhitePawns() >> 16) & b.getEmptyFields() & (b.getEmptyFields() >> 8) & RANK_4;//move 2 forward
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i / 8 + 2) + (i % 8) + (i / 8) + (i % 8);
+
+            //1 forward
+            long pawnMoves = (b.getWhitePawns() >> 8)
+                    & ~RANK_8
+                    & b.getEmptyFields();
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i / 8 + 1) + (i % 8) + (i / 8) + (i % 8);
                 }
             }
 
-            //TODO adjust promotion moves for game server implementation to source/target/piece
-            //y1,y2,Promotion Type,"P"
-            PAWN_MOVES = (b.getWhitePawns() >> 7) & (b.getBlackPieces() & ~b.getBlackKing()) & RANK_8 & ~FILE_A;//pawn promotion by capture right
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i % 8 - 1) + (i % 8) + "QP" + (i % 8 - 1) + (i % 8) + "RP" + (i % 8 - 1) + (i % 8) + "BP" + (i % 8 - 1) + (i % 8) + "NP";
+            //2 forward
+            pawnMoves = (b.getWhitePawns() >> 16)
+                    & RANK_4
+                    & b.getEmptyFields() & (b.getEmptyFields() >> 8) ;
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i / 8 + 2) + (i % 8) + (i / 8) + (i % 8);
                 }
             }
-            PAWN_MOVES = (b.getWhitePawns() >> 9) & (b.getBlackPieces() & ~b.getBlackKing()) & RANK_8 & ~FILE_H;//pawn promotion by capture left
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i % 8 + 1) + (i % 8) + "QP" + (i % 8 + 1) + (i % 8) + "RP" + (i % 8 + 1) + (i % 8) + "BP" + (i % 8 + 1) + (i % 8) + "NP";
+
+            //right capture
+            pawnMoves = (b.getWhitePawns() >> 7)
+                    & ~RANK_8 & ~FILE_A &
+                    (b.getBlackPieces() & ~b.getBlackKing());
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i / 8 + 1) + (i % 8 - 1) + (i / 8) + (i % 8);
                 }
             }
-            PAWN_MOVES = (b.getWhitePawns() >> 8) & b.getEmptyFields() & RANK_8;//pawn promotion by move 1 forward
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i % 8) + (i % 8) + "QP" + (i % 8) + (i % 8) + "RP" + (i % 8) + (i % 8) + "BP" + (i % 8) + (i % 8) + "NP";
+
+            //left capture
+            pawnMoves = (b.getWhitePawns() >> 9)
+                    & ~RANK_8 & ~FILE_H
+                    & (b.getBlackPieces() & ~b.getBlackKing());
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i / 8 + 1) + (i % 8 + 1) + (i / 8) + (i % 8);
                 }
             }
-            //y1,y2,Space,"WE"
-            //TODO: wenn enPassant Feld im FEN angegeben, vereinfachen evtl. möglich
+
+
+            //Promotions
+            //pawn promotion forward
+            pawnMoves = (b.getWhitePawns() >> 8)
+                    & RANK_8
+                    & b.getEmptyFields();
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i % 8) + (i % 8) + "QP"
+                            + (i % 8) + (i % 8) + "BP"
+                            + (i % 8) + (i % 8) + "RP"
+                            + (i % 8) + (i % 8) + "NP";
+                }
+            }
+
+            //pawn promotion, capture right
+            pawnMoves = (b.getWhitePawns() >> 7)
+                    & RANK_8 & ~FILE_A
+                    & (b.getBlackPieces() & ~b.getBlackKing());
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i % 8 - 1) + (i % 8) + "QP"
+                            + (i % 8 - 1) + (i % 8) + "BP"
+                            + (i % 8 - 1) + (i % 8) + "RP"
+                            + (i % 8 - 1) + (i % 8) + "NP";
+                }
+            }
+
+            //pawn promotion by capture left
+            pawnMoves = (b.getWhitePawns() >> 9) & (b.getBlackPieces() & ~b.getBlackKing()) & RANK_8 & ~FILE_H;
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i % 8 + 1) + (i % 8) + "QP"
+                            + (i % 8 + 1) + (i % 8) + "BP"
+                            + (i % 8 + 1) + (i % 8) + "RP"
+                            + (i % 8 + 1) + (i % 8) + "NP";
+                }
+            }
 
             long EPBitboardFileFromFenString = 0L;
             if(b.getEnPassants().length()>1){
                 EPBitboardFileFromFenString = FileMasks8[b.getEnPassants().charAt(0)-97];
-            } //TODO: ändern, wenn auf enPassant-Methode/FEN-Format geeinigt
-
-
-            long possibility = 0L; //TODO: entfernen, wenn Move-Optimation bei restlichen Pawn Moves -> schon deklariert
-            //alt: possibility = (b.getWhitePawns() << 1)&b.getBlackPawns()&RANK_5&~FILE_A&b.getEnPassantBitboardFile();//shows piece to remove, not the destination
-            possibility = (b.getWhitePawns() << 1)&b.getBlackPawns()&RANK_5&~FILE_A&EPBitboardFileFromFenString;
-            if (possibility != 0)
-            {
-                int index=Long.numberOfTrailingZeros(possibility);
-                list+=""+(index%8-1)+(index%8)+"WE";
             }
-            //en passant left
-            possibility = (b.getWhitePawns() >> 1)&b.getBlackPawns()&RANK_5&~FILE_H&EPBitboardFileFromFenString;//shows piece to remove, not the destination
-            if (possibility != 0)
+
+            long option = 0L;
+            option = (b.getWhitePawns() << 1)
+                    & RANK_5 & ~FILE_A
+                    & b.getBlackPawns() & EPBitboardFileFromFenString;
+            if (option != 0)
             {
-                int index=Long.numberOfTrailingZeros(possibility);
-                list+=""+(index%8+1)+(index%8)+"WE";
+                int index=Long.numberOfTrailingZeros(option);
+                moveList+=""+(index%8-1)+(index%8)+"WE";
+            }
+
+            //en passant left
+            option = (b.getWhitePawns() >> 1) & b.getBlackPawns()
+                    & RANK_5 & ~FILE_H
+                    & EPBitboardFileFromFenString;
+            if (option != 0)
+            {
+                int index=Long.numberOfTrailingZeros(option);
+                moveList+=""+(index%8+1)+(index%8)+"WE";
             }
 
         } else {
 
+            //move 1 forward
+            long pawnMoves = (b.getBlackPawns() << 8)
+                    & ~RANK_1
+                    & b.getEmptyFields();
 
-
-            //x1,y1,x2,y2
-            long PAWN_MOVES = (b.getBlackPawns() << 7) & (b.getWhitePieces() & ~b.getWhiteKing()) & ~RANK_1 & ~FILE_H;//capture right
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i / 8 - 1) + (i % 8 + 1) + (i / 8) + (i % 8);
-                }
-            }
-            PAWN_MOVES = (b.getBlackPawns() << 9) & (b.getWhitePieces() & ~b.getWhiteKing()) & ~RANK_1 & ~FILE_A;//capture left
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i / 8 - 1) + (i % 8 - 1) + (i / 8) + (i % 8);
-                }
-            }
-            PAWN_MOVES = (b.getBlackPawns() << 8) & b.getEmptyFields() & ~RANK_1;//move 1 forward
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i / 8 - 1) + (i % 8) + (i / 8) + (i % 8);
-                }
-            }
-            PAWN_MOVES = (b.getBlackPawns() << 16) & b.getEmptyFields() & (b.getEmptyFields() << 8) & RANK_5;//move 2 forward
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i / 8 - 2) + (i % 8) + (i / 8) + (i % 8);
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i / 8 - 1) + (i % 8) + (i / 8) + (i % 8);
                 }
             }
 
-            //y1,y2,Promotion Type,"P"
-            PAWN_MOVES = (b.getBlackPawns() << 7) & (b.getWhitePieces() & ~b.getWhiteKing()) & RANK_1 & ~FILE_H;//pawn promotion by capture right
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i % 8 + 1) + (i % 8) + "qP" + (i % 8 + 1) + (i % 8) + "rP" + (i % 8 + 1) + (i % 8) + "bP" + (i % 8 + 1) + (i % 8) + "nP";
-                }
-            }
-            PAWN_MOVES = (b.getBlackPawns() << 9) & (b.getWhitePieces() & ~b.getWhiteKing()) & RANK_1 & ~FILE_A;//pawn promotion by capture left
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i % 8 - 1) + (i % 8) + "qP" + (i % 8 - 1) + (i % 8) + "rP" + (i % 8 - 1) + (i % 8) + "bP" + (i % 8 - 1) + (i % 8) + "nP";
-                }
-            }
-            PAWN_MOVES = (b.getBlackPawns() << 8) & b.getEmptyFields() & RANK_1;//pawn promotion by move 1 forward
-            for (int i = Long.numberOfTrailingZeros(PAWN_MOVES); i < 64 - Long.numberOfLeadingZeros(PAWN_MOVES); i++) {
-                if (((PAWN_MOVES >> i) & 1) == 1) {
-                    list += "" + (i % 8) + (i % 8) + "qP" + (i % 8) + (i % 8) + "rP" + (i % 8) + (i % 8) + "bP" + (i % 8) + (i % 8) + "nP";
-                }
-            }
-            //y1,y2,"BE"//TODO: check if bE or BE is correct
 
-            long EPBitboardFileFromFenString = 0L;
+            //move 2 forward
+            pawnMoves = (b.getBlackPawns() << 16)
+                    & RANK_5
+                    & b.getEmptyFields() & (b.getEmptyFields() << 8);
+
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i / 8 - 2) + (i % 8) + (i / 8) + (i % 8);
+                }
+            }
+
+            //capture right
+            pawnMoves = (b.getBlackPawns() << 7)
+                    & ~RANK_1 & ~FILE_H
+                    & (b.getWhitePieces() & ~b.getWhiteKing());
+
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i / 8 - 1) + (i % 8 + 1) + (i / 8) + (i % 8);
+                }
+            }
+
+
+            //capture left
+            pawnMoves = (b.getBlackPawns() << 9)
+                    & ~RANK_1 & ~FILE_A
+                    & (b.getWhitePieces() & ~b.getWhiteKing());
+
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += "" + (i / 8 - 1) + (i % 8 - 1) + (i / 8) + (i % 8);
+                }
+            }
+
+
+            //Promotions
+            //pawn promotion forward
+            pawnMoves = (b.getBlackPawns() << 8)
+                    & RANK_1
+                    & b.getEmptyFields();
+
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += ""
+                            + (i % 8) + (i % 8) + "qP"
+                            + (i % 8) + (i % 8) + "rP"
+                            + (i % 8) + (i % 8) + "bP"
+                            + (i % 8) + (i % 8) + "nP";
+                }
+            }
+
+
+            //pawn promotion, capture left
+            pawnMoves = (b.getBlackPawns() << 9)
+                    & RANK_1 & ~FILE_A
+                    & (b.getWhitePieces() & ~b.getWhiteKing());
+
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += ""
+                            + (i % 8 - 1) + (i % 8) + "qP"
+                            + (i % 8 - 1) + (i % 8) + "rP"
+                            + (i % 8 - 1) + (i % 8) + "bP"
+                            + (i % 8 - 1) + (i % 8) + "nP";
+                }
+            }
+
+
+            //pawn promotion, capture right
+            pawnMoves = (b.getBlackPawns() << 7) & (b.getWhitePieces() & ~b.getWhiteKing()) & RANK_1 & ~FILE_H;
+            for (int i = Long.numberOfTrailingZeros(pawnMoves); i < 64 - Long.numberOfLeadingZeros(pawnMoves); i++) {
+                if (((pawnMoves >> i) & 1) == 1) {
+                    moveList += ""
+                            + (i % 8 + 1) + (i % 8) + "qP"
+                            + (i % 8 + 1) + (i % 8) + "rP"
+                            + (i % 8 + 1) + (i % 8) + "bP"
+                            + (i % 8 + 1) + (i % 8) + "nP";
+                }
+            }
+
+
+            long EpColumn = 0L;
             if(b.getEnPassants().length()>1){
-                EPBitboardFileFromFenString = FileMasks8[b.getEnPassants().charAt(0)-97];
+                EpColumn = FileMasks8[b.getEnPassants().charAt(0)-97];
             }
 
+            //en passant right
+            long option = 0L;
+            option = (b.getBlackPawns() >> 1)&b.getWhitePawns()
+                    & RANK_4 & ~FILE_H
+                    & EpColumn;
 
-
-            long possibility = 0L; //TODO: entfernen, wenn Move-Optimation bei restlichen Pawn Moves -> schon deklariert
-            possibility = (b.getBlackPawns() >> 1)&b.getWhitePawns()&RANK_4&~FILE_H&EPBitboardFileFromFenString;//shows piece to remove, not the destination
-            if (possibility != 0)
+            if (option != 0)
             {
-                int index=Long.numberOfTrailingZeros(possibility);
-                list+=""+(index%8+1)+(index%8)+"BE";
+                int index=Long.numberOfTrailingZeros(option);
+                moveList+=""+(index%8+1)+(index%8)+"BE";
             }
+
             //en passant left
-            possibility = (b.getBlackPawns() << 1)&b.getWhitePawns()&RANK_4&~FILE_A&EPBitboardFileFromFenString;//shows piece to remove, not the destination
-            if (possibility != 0)
-            {
-                int index=Long.numberOfTrailingZeros(possibility);
-                list+=""+(index%8-1)+(index%8)+"BE";
-            }
+            option = (b.getBlackPawns() << 1)&b.getWhitePawns()
+                    & RANK_4 & ~FILE_A
+                    &EpColumn;
 
+            if (option != 0)
+            {
+                int index=Long.numberOfTrailingZeros(option);
+                moveList+=""+(index%8-1)+(index%8)+"BE";
+            }
         }
 
-        return list;
+        return moveList;
 
     }
 }
