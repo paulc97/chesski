ff023a3bd50f6c2546b8f2b994497cec291bad9f
cecik <cecilie.kudlek@gmail.com>
2022-07-28 15:56:48 +0200

    added new Test classes

---
+++ Main/java/tests/Tests.java
@@ -0,0 +1,21 @@
+import Model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static Model.MoveGenerator.getQuiescenceSearchIterations;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Tests {
+    Board board;
+    MoveGenerator moveGenerator;
+
+    @BeforeEach
+    void setUp() {
+        //board = new Board("rnbqkbnr/1p1pp1p1/p1p2p1p/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1");
+        board = new Board("7r/Pn6/8/8/8/8/2K4p/k1n5 w KQkq - 4 265");
+        moveGenerator = new MoveGenerator();
+    }
+}
